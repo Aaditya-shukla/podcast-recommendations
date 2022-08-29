ae0bb5a3cc1dce9f69195da3ab64e7372a8f623a
Aaditya-shukla <aadityashukla9@hotmail.com>
2022-08-29 13:39:21 +0530

    Changing redis conf

--- redis.js
+++ redis.js
@@ -1,15 +1,17 @@
 const redis = require('redis');
 
 const client = redis.createClient({
-    // socket: {
-    //     host: 'redis-13657.c273.us-east-1-2.ec2.cloud.redislabs.com',
-    //     port: 13657
-    // },
-    // password: 'ev96usoRu1akKPFVDz9CgP0RK7fRfizG'
-       socket: {
+     socket: {
+         host: 'redis-13657.c273.us-east-1-2.ec2.cloud.redislabs.com',
+         port: 13657
+     },
+     password: 'ev96usoRu1akKPFVDz9CgP0RK7fRfizG'
+/*     
+  socket: {
         host: 'localhost',
         port: 6379
     }
+*/
    
 });
 
